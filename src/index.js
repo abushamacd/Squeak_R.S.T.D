1f0b946bd30a8a9416aa024395b7b5b85cf6ea01
Shama <assiddik001@gmail.com>
2022-11-21 02:53:27 +0600

    add syncfushion licence

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import { registerLicense } from "@syncfusion/ej2-base";
+registerLicense(process.env.REACT_APP_SF_KEY);
 
 ReactDOM.render(
   <ContextProvider>
