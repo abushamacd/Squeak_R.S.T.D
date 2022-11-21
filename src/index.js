50242de7d3ba5c8ffd993ea2815edd13a4c221d5
Shama <assiddik001@gmail.com>
2022-11-21 21:11:08 +0600

    remore react error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 import { registerLicense } from "@syncfusion/ej2-base";
 registerLicense(process.env.REACT_APP_SF_KEY);
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <ContextProvider>
     <App />
-  </ContextProvider>,
-  document.getElementById("root")
+  </ContextProvider>
 );
