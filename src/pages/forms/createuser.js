666f43172ffc151a21baff51a4493981cadf6699
Shama <assiddik001@gmail.com>
2022-11-21 19:07:43 +0600

    create user form

--- src/pages/forms/createuser.js
+++ src/pages/forms/createuser.js
@@ -1,10 +1,176 @@
 import React from "react";
 import Header from "../../components/Header";
+import { useState } from "react";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Createuser = () => {
+  const [file, setFile] = useState("");
+  const { currentColor } = useStateContext();
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Page" title="Create User" />
+      <section className="max-w-4xl p-6 pt-0 mx-auto bg-white text-black rounded-md shadow-md dark:bg-secondary-dark-bg mt-20">
+        <h1 className="text-xl font-bold text-black capitalize dark:text-white">
+          Account settings
+        </h1>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="emailAddress"
+              >
+                Email Address
+              </label>
+              <input
+                id="emailAddress"
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Password Confirmation
+              </label>
+              <input
+                id="passwordConfirmation"
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Date
+              </label>
+              <input
+                id="date"
+                type="date"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label
+                className="text-black dark:text-gray-200"
+                htmlFor="passwordConfirmation"
+              >
+                Text Area
+              </label>
+              <textarea
+                id="textarea"
+                type="textarea"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              ></textarea>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-black">
+                Image
+              </label>
+              <div className="mt-1 flex justify-center dark:text-white px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                <div className="space-y-1 text-center">
+                  <svg
+                    className="mx-auto h-12 w-12 text-black dark:text-white"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 48 48"
+                    aria-hidden="true"
+                  >
+                    <path
+                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                  <div className="flex text-sm text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500 p-1"
+                    >
+                      <span className="">Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        onChange={(e) => setFile(e.target.files[0])}
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1 text-black dark:text-white">
+                      or drag and drop
+                    </p>
+                  </div>
+                  <p className="text-xs text-black dark:text-white">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="rounded-full">
+              <label className="block text-sm font-medium text-black">
+                Preview
+              </label>
+              <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 rounded-md">
+                <div className="space-y-1 text-center">
+                  <img
+                    width={200}
+                    height={200}
+                    src={
+                      file
+                        ? URL.createObjectURL(file)
+                        : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+                    }
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-6">
+            <button
+              style={{ backgroundColor: currentColor }}
+              className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform rounded-md focus:outline-none focus:bg-gray-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   );
 };
